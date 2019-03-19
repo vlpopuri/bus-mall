2ce0ebeac370d88273e6cbfb75f89c96eb48b88c
vijaya <vlpopuri@hotmail.com>
2019-03-19 15:09:34 -0700

    add chart

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ var allImages = [];
 var leftImageThatIsOnThePage;
 var centerImageThatIsOnThePage;
 var rightImageThatIsOnThePage;
+var previouslyPickedImages=[];
 
 // references to the DOM (html) document object model
 var leftImage = document.getElementById('left-image');
@@ -44,12 +45,14 @@ var rightImage = document.getElementById('right-image');
 
 //Intialize the page
 
-var leftDiv = document.getElementById('left-div');
-console.log(leftDiv);
-var centerDiv = document.getElementById('center-div');
-console.log(centerDiv);
-var rightDiv = document.getElementById('right-div');
-console.log(rightDiv);
+var all_ProductImagesContainerSectionEl = document.getElementById('all_product_Images');
+
+// var leftDiv = document.getElementById('left-div');
+// console.log(leftDiv);
+// var centerDiv = document.getElementById('center-div');
+// console.log(centerDiv);
+// var rightDiv = document.getElementById('right-div');
+// console.log(rightDiv);
 
 //declarting the constructors
 var BusCatalogImage = function(src, name){
@@ -60,8 +63,9 @@ var BusCatalogImage = function(src, name){
   allImages.push(this);
 };
 
-
-
+//=============================================================
+// Other functions
+//=================================================================
 //    randomly select an image(three images)
 //    - random number generator - to pick the goat
 //    - change the DOM
@@ -74,6 +78,8 @@ function handleClickOnLeftImage(event){
   // increment left Image clicks
   leftImageThatIsOnThePage.clicks++;
 
+  //selecting 3 random images
+  //TODO: make sure you dont repeat
   var leftImageIndex = Math.floor(Math.random() * allImages.length);
   var centerImageIndex = Math.floor( Math.random() * allImages.length );
   var rightImageIndex = Math.floor(Math.random() * allImages.length);
@@ -90,7 +96,7 @@ function handleClickOnLeftImage(event){
 
 
   // stop after 2 clicks
-  if(likeCounter > 2 ){
+  if(likeCounter > 5){
     // stop listening for clicks on the left, center and right images
     leftDiv.removeEventListener('click', handleClickOnLeftImage);
     centerDiv.removeEventListener('click', handleClickOnCenterImage);
@@ -116,13 +122,13 @@ function handleClickOnCenterImage(event) {
   rightImageThatIsOnThePage = allImages[rightImageIndex];
 
   //and put them on the page
-  
+
   leftImage.src = leftImageThatIsOnThePage.imagesrc;
   centerImage.src = centerImageThatIsOnThePage.imagesrc;
   rightImage.src = rightImageThatIsOnThePage.imagesrc;
 
   // stop after 2 clicks
-  if (likeCounter > 2) {
+  if (likeCounter > 5) {
     // stop listening for clicks on the left,center and right images
     leftDiv.removeEventListener('click', handleClickOnLeftImage);
     centerDiv.removeEventListener('click', handleClickOnCenterImage);
@@ -137,7 +143,7 @@ function handleClickOnRightImage(event) {
   likeCounter++;
   // increment right goat's clicks
   rightImageThatIsOnThePage.clicks++;
-  
+
   var leftImageIndex = Math.floor(Math.random() * allImages.length);
   var centerImageIndex = Math.floor( Math.random() * allImages.length );
   var rightImageIndex = Math.floor(Math.random() * allImages.length);
@@ -151,7 +157,7 @@ function handleClickOnRightImage(event) {
   centerImage.src = centerImageThatIsOnThePage.imagesrc;
   rightImage.src = rightImageThatIsOnThePage.imagesrc;
 
-  if (likeCounter > 2) {
+  if (likeCounter > 5) {
     // stop listening for clicks on the left,center and right images
     leftDiv.removeEventListener('click', handleClickOnLeftImage);
     centerDiv.removeEventListener('click', handleClickOnCenterImage);
@@ -171,25 +177,71 @@ new BusCatalogImage('./images/bathroom.jpg', 'Bathroom');
 new BusCatalogImage('./images/boots.jpg', 'Boots');
 new BusCatalogImage('./images/breakfast.jpg', 'Breakfast');
 new BusCatalogImage('./images/bubblegum.jpg', 'Bubbegum');
-new BusCatalogImage('./images/chair.jpg', 'Chair');
-new BusCatalogImage('./images/cthulhu.jpg', 'Cthulhu');
-new BusCatalogImage('./images/dog-duck.jpg', 'Dog-Duck');
-new BusCatalogImage('./images/dragon.jpg', 'Dragon');
-new BusCatalogImage('./images/pen.jpg', 'Pen');
-new BusCatalogImage('./images/scissors.jpg', 'Scissors');
-new BusCatalogImage('./images/shark.jpg', 'Shark');
-new BusCatalogImage('./images/sweep.png', 'Sweep');
-new BusCatalogImage('./images/tauntaun.jpg', 'TaunTaun');
-new BusCatalogImage('./images/unicorn.jpg', 'Unicorn');
-new BusCatalogImage('./images/usb.gif', 'Usb');
-new BusCatalogImage('./images/water-can.jpg', 'Water-Can');
+// new BusCatalogImage('./images/chair.jpg', 'Chair');
+// new BusCatalogImage('./images/cthulhu.jpg', 'Cthulhu');
+// new BusCatalogImage('./images/dog-duck.jpg', 'Dog-Duck');
+// new BusCatalogImage('./images/dragon.jpg', 'Dragon');
+// new BusCatalogImage('./images/pen.jpg', 'Pen');
+// new BusCatalogImage('./images/scissors.jpg', 'Scissors');
+// new BusCatalogImage('./images/shark.jpg', 'Shark');
+// new BusCatalogImage('./images/sweep.png', 'Sweep');
+// new BusCatalogImage('./images/tauntaun.jpg', 'TaunTaun');
+// new BusCatalogImage('./images/unicorn.jpg', 'Unicorn');
+// new BusCatalogImage('./images/usb.gif', 'Usb');
+// new BusCatalogImage('./images/water-can.jpg', 'Water-Can');
 
 
 leftImageThatIsOnThePage= allImages[2];
 centerImageThatIsOnThePage=allImages[1];
 rightImageThatIsOnThePage=allImages[0];
 
+//================================================================
+//chart added to show up the items
+//================================================================
 
+
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Travel Bag', 'Banana', 'Bathroom', 'Boots', 'Breakfast', 'Bubblegum'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    animation: {
+      duration: 10000,
+      easing: 'easeInQuad'
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
 
 
 
